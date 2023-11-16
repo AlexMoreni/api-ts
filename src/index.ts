cffa82eb88b7ab77c8be9e03fd98745915e47b6d
AlexMoreni <alex.moreni47@gmail.com>
2023-11-16 14:16:04 -0300

    authentication route and error correction when importing

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import router from "./router";
+
 const app = express();
 
 app.use(
@@ -24,9 +26,10 @@ server.listen(8080, () => {
   console.log(`Server running on http://localhost:8080/`);
 });
 
-const MONGO_URL =
-  "mongodb+srv://alex:12345@cluster0.dtn732b.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URL = "mongodb+srv://alex:12345@cluster0.dtn732b.mongodb.net/";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
